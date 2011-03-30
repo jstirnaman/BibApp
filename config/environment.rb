24d53dd2e7261c67f9da2cc966b07c26e4ce4313
Jason Stirnaman <stirnamanj@gmail.com>
2011-03-30 12:31:44 -0500

    moved Solr multicore settings from solr.rb to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,2 +1,13 @@
 require File.expand_path('../application', __FILE__)
 Bibapp::Application.initialize!
+
+# Override solr.rb settings in favor of Solr Multicore
+# Solr core to use. Default to core1
+SOLR_CORE = ENV['CORE_N'] || 1
+SOLR_CORE = "1" unless defined? SOLR_CORE
+
+# Build our Solr URL for Multicore setup
+SOLR_URL = "http://localhost:#{SOLR_PORT}/solr/#{[Rails.env]}-core#{SOLR_CORE}"
+
+# Solr Connection (used by /app/models/index.rb)
+SOLRCONN = Solr::Connection.new(SOLR_URL)
