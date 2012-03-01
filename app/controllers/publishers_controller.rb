51d849bfbab49d0e27edaf6621d97d59d74aec3c
Howard Ding <hding2@illinois.edu>
2012-03-01 15:34:23 -0600

    Cache rendering of shared/sherpa_icon in index views and make other appropriate changes to related views/partials. Some other efficiency measures - some of the displayed data we can get pretty easily from the database without instantiating objects. It may be possible to streamline this further by doing all the necessary work in a single query, but even doing many queries speeds things up significantly compared to instantiating the objects.

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -43,8 +43,7 @@ class PublishersController < ApplicationController
         @current_objects = current_objects
       else
         @page = params[:page] || @a_to_z[0]
-
-        @current_objects = Publisher.includes(:publications => :works).authorities.sort_name_like("#{@page}%").order('sort_name')
+        @current_objects = Publisher.authorities.sort_name_like("#{@page}%").order('sort_name')
       end
     end
 
@@ -93,8 +92,7 @@ class PublishersController < ApplicationController
     else
       @page = params[:page] || @a_to_z[0]
       @current_objects = Publisher.authorities.sort_name_like("#{@page}%").order(:sort_name).
-
-              includes(:publications, :publisher_source)
+              includes(:publications)
     end
 
     #Keep a list of publications in process in session[:publication_auths]
