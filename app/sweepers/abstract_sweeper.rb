2d66d20cb7ff40bb0e68492a0fea5a7fa8831a84
Howard Ding <hding2@illinois.edu>
2012-03-07 12:00:58 -0600

    Some refactoring on determine expiration.

--- app/sweepers/abstract_sweeper.rb
+++ app/sweepers/abstract_sweeper.rb
@@ -4,11 +4,6 @@ class AbstractSweeper < ActionController::Caching::Sweeper
 
   #common pattern for determining if a cache expiration needs to happen
   #if any of the supplied methods or :destroyed? is true when called on record return true, else return false
-
-  def trigger_expiration?(record, *methods)
-    (methods << :destroyed?).detect {|m| record.send(m)}
-  end
-
   #if the given record and methods should cause a cache expiration as determined by trigger_expiration?
   #then yield to the block, which should compute the relevant ids and return an array (returned as is), a false value
   #(returned as an empty array), or a single id (returned as a one element array)
