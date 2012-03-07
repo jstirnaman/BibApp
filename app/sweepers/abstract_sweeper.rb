95a0b94ab3ac6c86384d3eeeba79870f769ccdcd
Howard Ding <hding2@illinois.edu>
2012-03-07 12:18:11 -0600

    Refactored. We have a general method that can take the record passed to the sweeper and call a lot of methods on it to determine if something has happened that should require expiration. We then have another method that uses that and a supplied block to figure out what ids to expire and return as an array. This simplifies the code in the actual sweeper.

--- app/sweepers/abstract_sweeper.rb
+++ app/sweepers/abstract_sweeper.rb
@@ -4,6 +4,24 @@ class AbstractSweeper < ActionController::Caching::Sweeper
 
   #common pattern for determining if a cache expiration needs to happen
   #if any of the supplied methods or :destroyed? is true when called on record return true, else return false
+<<<<<<< HEAD
+  #if the given record and methods should cause a cache expiration as determined by trigger_expiration?
+  #then yield to the block, which should compute the relevant ids and return an array (returned as is), a false value
+  #(returned as an empty array), or a single id (returned as a one element array)
+  #if not then return an empty array
+  def expired_ids(record, *methods)
+    if trigger_expiration?(record, *methods)
+      ids = yield
+      ids.is_a?(Array) ? ids : (ids ? [ids] : [])
+    else
+      []
+    end
+=======
+  def trigger_expiration?(record, *methods)
+    (methods << :destroyed?).detect {|m| record.send(m)}
+>>>>>>> Refactored. We have a general method that can take the record
+  end
+
   #if the given record and methods should cause a cache expiration as determined by trigger_expiration?
   #then yield to the block, which should compute the relevant ids and return an array (returned as is), a false value
   #(returned as an empty array), or a single id (returned as a one element array)
