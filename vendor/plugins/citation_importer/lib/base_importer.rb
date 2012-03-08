623dbe657c79a6dafed85310aa976841e16f3b94
Howard Ding <hding2@illinois.edu>
2012-03-08 11:21:01 -0600

    move methods to base class and slightly refactor

--- vendor/plugins/citation_importer/lib/base_importer.rb
+++ vendor/plugins/citation_importer/lib/base_importer.rb
@@ -239,4 +239,12 @@ class BaseImporter < CitationImporter
     source_keys.each { |key| hash.delete(key) }
   end
 
+  def strip_line_breaks(value)
+    value.mb_chars.squish
+  end
+
+  def remove_trailing_period(value)
+    value.gsub(/\.(\s*)$/, "")
+  end
+
 end
