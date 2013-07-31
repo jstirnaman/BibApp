923d0a7181dbd3bbe88bc0e103a498ff31a1bfe3
Jason Stirnaman <jstirnaman@kumc.edu>
2013-07-31 19:45:27 +0000

    Enable exception notifications.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,13 +13,13 @@ Bibapp::Application.configure do
   config.action_controller.perform_caching = true
   config.action_controller.cache_store = :file_store, File.join(Rails.root, 'tmp', 'cache', Rails.env)
 
-# Notify administrator of exceptions
-#config.middleware.use ExceptionNotification::Rack,
-#  :email => {
-#    :email_prefix => "[BibApp Exception] ",
-#    :sender_address => %{"notifier" <experts-no-reply@kumc.edu>},
-#    :exception_recipients => %w{jstirnaman@kumc.edu, eteasdale@kumc.edu}
-#  }
+# Notify administrator(s) of exceptions. Multiple recipients can be specified as a comma-delimited list.
+config.middleware.use ExceptionNotification::Rack,
+  :email => {
+    :email_prefix => "[BibApp Exception] ",
+    :sender_address => %{"notifier" <experts-no-reply@kumc.edu>},
+    :exception_recipients => %w{jstirnaman@kumc.edu, eteasdale@kumc.edu}
+  }
 
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # config.action_controller.asset_host                  = "http://assets.example.com"
@@ -32,6 +32,6 @@ Bibapp::Application.configure do
   }
 
 # Uncomment if you need to see something running in production but with more logging
-  config.log_level = :debug
+#  config.log_level = :debug
 
 end
